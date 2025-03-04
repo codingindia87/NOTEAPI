67ffa685262bbb40efb5a2d6eb3848330b41d4d6
codingindia87 <tachs098@gmail.com>
2025-03-04 16:26:51 +0530

    new2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const userRouters = require("./routes/userRouters");
-const noteRouters = require("./routes/noteRouters");
-const app = express();
+const userRouters = require("./src/routes/userRouters");
+const noteRouters = require("./src/routes/noteRouters");
 const mongoose = require("mongoose");
 const cors = require("cors")
-
 const dotenv = require("dotenv")
 
 dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 
@@ -16,17 +16,18 @@ app.use(cors());
 app.use("/users",userRouters);
 app.use("/note",noteRouters);
 
-mongoose.connect(process.env.MONGO_URL)
-
-console.log("hello")
-
 const PORT = process.env.PORT || 5000
+
+
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     app.listen(PORT,()=>{
         console.log("Server startd on port on: " + PORT);
     })
-})
-.catch((error)=>{
+}).catch((error)=>{
     console.log(error);
 })
+
+console.log("hello")
+
 
