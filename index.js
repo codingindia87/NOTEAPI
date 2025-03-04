ba606cdc2b315d5a411e23f7fd13845d27e2b30b
codingindia87 <tachs098@gmail.com>
2025-03-04 22:11:04 +0530

    new5

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require("express");
-const userRouters = require("./src/routes/userRouters");
-const noteRouters = require("./src/routes/noteRouters");
-const mongoose = require("mongoose");
-const cors = require("cors")
-const dotenv = require("dotenv")
+// const userRouters = require("./src/routes/userRouters");
+// const noteRouters = require("./src/routes/noteRouters");
+// const mongoose = require("mongoose");
+// const cors = require("cors")
+// const dotenv = require("dotenv")
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 
-app.use("/users",userRouters);
-app.use("/note",noteRouters);
+// app.use("/users",userRouters);
+// app.use("/note",noteRouters);
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server startd on port on: " + PORT);
-    })
-}).catch((error)=>{
-    console.log(error);
-})
+// mongoose.connect(process.env.MONGO_URL)
+// .then(()=>{
+//     app.listen(PORT,()=>{
+//         console.log("Server startd on port on: " + PORT);
+//     })
+// }).catch((error)=>{
+//     console.log(error);
+// })
 
 console.log("hello")
 
